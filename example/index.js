c63c52c7d63fe8cc08f79d576308b1bd7d6029cd
Ionică Bizău <bizauionica@gmail.com>
2015-11-18 19:11:02 +0200

    Added blah config
    Credits to @travist for the amazing library he built. :grin:

--- example/index.js
+++ example/index.js
@@ -27,12 +27,13 @@ phJQuery.open("http://ionicabizau.net", (err, $, page, ph) => {
         ph.exit();
     });
 });
- // => Index: 0 [Blog](/)
- // => Index: 1 [About](/about)
- // => Index: 2 [FAQ](/faq)
- // => Index: 3 [Contact](/contact)
- // => Index: 4 [How to convert JSON to Markdown using json2md](/blog/27-how-to-convert-json-to-markdown-using-json2md)
- // ...
- // => Index: 27 [](http://bitbucket.com/IonicaBizau)
- // => Index: 28 [](http://twitter.com/IonicaBizau)
- // => Index: 29 [](http://youtube.com/IonicaBizau)
+
+// => Index: 0 [Blog](/)
+// => Index: 1 [About](/about)
+// => Index: 2 [FAQ](/faq)
+// => Index: 3 [Contact](/contact)
+// => Index: 4 [How to convert JSON to Markdown using json2md](/blog/27-how-to-convert-json-to-markdown-using-json2md)
+// ...
+// => Index: 27 [](http://bitbucket.com/IonicaBizau)
+// => Index: 28 [](http://twitter.com/IonicaBizau)
+// => Index: 29 [](http://youtube.com/IonicaBizau)
